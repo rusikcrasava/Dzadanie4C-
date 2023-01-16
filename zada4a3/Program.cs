5bd35198bc765d00d62ba2ca4d13402f2c66305b
rusikcrasava <rusikcrasava@mail.ru>
2023-01-16 15:07:22 +0300

    Третья задача выполнена

---
+++ zada4a3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Начало создания массива длиной A в диапозоне 0-A");
+            Console.Write("Введите любое число (A): ");
+            int enterNumber = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[enterNumber];
+
+            for (int i = 0; i < enterNumber; i++)
+            {
+                array[i] = new Random().Next(enterNumber+1);
+                Console.Write(array[i] + "; ");
+            }
+            Console.WriteLine();
+
