53f0792eee3dce085e699072894a22707b4b4986
rusikcrasava <rusikcrasava@mail.ru>
2023-01-16 14:58:14 +0300

    Первая задача сделана

---
+++ zada4a1/Program.cs
@@ -0,0 +1,22 @@
+﻿int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+Stepen(numberA, numberB);
+
+
+// Функция возведения в степень
+void Stepen(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
