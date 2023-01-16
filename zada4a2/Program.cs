a809719f7c42adc2ddfcd586fcda1da18ca97b69
rusikcrasava <rusikcrasava@mail.ru>
2023-01-16 15:04:13 +0300

    Вторая задача выполнена

---
+++ zada4a2/Program.cs
@@ -0,0 +1,10 @@
+﻿ Console.Write("Введите любое число: ");
+            int enterNumber = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            while (enterNumber != 0)
+            {
+                result += enterNumber % 10;
+                enterNumber /= 10;
+            }
+
+            Console.WriteLine($"Сумма цифр в числе = {result}");
